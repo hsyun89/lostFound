11980f2adb3e4a904cb21e690d438defa8fa77ef
kyungJun <vndn337@nate.com>
2019-09-26 17:42:03 +0900

    listAll, listOne

---
+++ src/main/java/dao/LostMyBatisDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import vo.LostVO;
+@Repository
+public class LostMyBatisDAO implements LostDAO{
+	@Autowired
+	SqlSessionFactory sqlSessionFactory;
+	public List<LostVO> listAll(){
+		List<LostVO> list=null;
+		SqlSession session = sqlSessionFactory.openSession();	
+		String statement = "LostMapper.selectLost";
+		list = session.selectList(statement);
+	    session.close();
+		return list;
+	}
+	public LostVO listOne(String actid){
+		LostVO list=null;
+		SqlSession session = sqlSessionFactory.openSession();	
+		String statement = "LostMapper.selectLostOne";
+		list = session.selectOne(statement, actid);
+	    session.close();
+		return list;
+	}
+	public List<LostVO> search(String keyword){
+		List<LostVO> list=null;
+		SqlSession session = sqlSessionFactory.openSession();	
+		String statement = "LostMapper.searchLost";
+		list = session.selectList(statement, keyword);
+		session.close();
+		return list;
+	}
+}
--- src/main/java/my/spring/mine/LostController.java
+++ src/main/java/my/spring/mine/LostController.java
@@ -1,17 +1,28 @@
 package my.spring.mine;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import dao.LostDAO;
+
 @Controller
 public class LostController {
+	@Autowired
+	LostDAO dao;
 	@RequestMapping(value = "/lost", method = RequestMethod.GET)
-	protected ModelAndView get() {
+	protected ModelAndView select(String atcid, String action, String keyword) {
 		ModelAndView mav = new ModelAndView();
-		String viewName = "lostView";
-		mav.setViewName(viewName);
+		if(atcid==null && action==null) {
+			mav.addObject("list", dao.listAll());
+		}else if(action.equals("read")) {
+			mav.addObject("listOne", dao.listOne(atcid));
+		}else if(action.equals("search")) {
+			mav.addObject("list", dao.search(keyword));
+		}
+		mav.setViewName("lostView");
 		return mav;
 	}
 }
