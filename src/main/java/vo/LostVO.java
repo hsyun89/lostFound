b3cd8b07014823a01643656f6859e64e2dacdf06
kkssomm <asdfx1415@naver.com>
2019-09-27 11:48:08 +0900

    db backup and lost_list

---
+++ src/main/java/vo/LostVO.java
@@ -0,0 +1,81 @@
+package vo;
+
+public class LostVO {
+	private String Addr;
+	private String Unique_id;
+	private String Content;
+	private String Keep_place;
+	private String Image_address;
+	private String Product_name;
+	private String Find_date;
+	private String Category;
+	private Double lon;
+	private Double lat;
+	public String getAddr() {
+		return Addr;
+	}
+	public void setAddr(String addr) {
+		Addr = addr;
+	}
+	public String getUnique_id() {
+		return Unique_id;
+	}
+	public void setUnique_id(String unique_id) {
+		Unique_id = unique_id;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public String getKeep_place() {
+		return Keep_place;
+	}
+	public void setKeep_place(String keep_place) {
+		Keep_place = keep_place;
+	}
+	public String getImage_address() {
+		return Image_address;
+	}
+	public void setImage_address(String image_address) {
+		Image_address = image_address;
+	}
+	public String getProduct_name() {
+		return Product_name;
+	}
+	public void setProduct_name(String product_name) {
+		Product_name = product_name;
+	}
+	public String getFind_date() {
+		return Find_date;
+	}
+	public void setFind_date(String find_date) {
+		Find_date = find_date;
+	}
+	public String getCategory() {
+		return Category;
+	}
+	public void setCategory(String category) {
+		Category = category;
+	}
+	public Double getLon() {
+		return lon;
+	}
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+	public Double getLat() {
+		return lat;
+	}
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+	@Override
+	public String toString() {
+		return "LostVO [Addr=" + Addr + ", Unique_id=" + Unique_id + ", Content=" + Content + ", Keep_place="
+				+ Keep_place + ", Image_address=" + Image_address + ", Product_name=" + Product_name + ", Find_date="
+				+ Find_date + ", Category=" + Category + ", lon=" + lon + ", lat=" + lat + "]";
+	}
+
+}
--- src/main/webapp/WEB-INF/views/lostView.jsp
+++ src/main/webapp/WEB-INF/views/lostView.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="vo.LostVO, java.util.ArrayList" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,52 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>찾기</h1>
+<%
+	ArrayList<LostVO> list = (ArrayList<LostVO>)request.getAttribute("list");
+	if(list != null) {
+%>
+	<h2>분실물 찾기</h2>
+	<table>
+	<tr>
+	<td>관리번호</td>
+	<td>습득물명</td>
+	<td>보관장소</td>
+	<td>습득일자</td>
+	</tr>
+<% 
+	for(LostVO vo : list) {
+%>
+	<tr>
+	<td><%= vo.getUnique_id()%></td>
+	<td class='<%=vo.getUnique_id()%>'><a href='/mine/lost?action=read&atcid=<%=vo.getUnique_id()%>'><%=vo.getProduct_name()%></a></td>
+	<td><%= vo.getKeep_place() %></td>
+	<td><%= vo.getFind_date() %></td>
+	</tr>
+<%
+	}
+%>
+	</table>
+<%
+	}else{
+%>
+<% 
+		LostVO listOne = (LostVO)request.getAttribute("listOne");
+%>
+	<h2>분실물 상세조회</h2>
+	<form method="post" action="/mine/lost">
+		<input type="hidden" name="actid" value="<%=listOne.getUnique_id()%>">
+		<input value="<%=listOne.getImage_address()%>">
+		<input value="습득물명<%=listOne.getProduct_name() %>">
+		<input type="text" value="관리번호<%=listOne.getUnique_id()%>">
+		<input type="text" value="습득일자<%=listOne.getFind_date()%>">
+		<input type="text" value="물품분류<%=listOne.getCategory()%>">
+		<input type="text" value="보관장소<%=listOne.getKeep_place()%>">
+		<hr>
+		<input type="text" value="<%=listOne.getContent()%>">
+		<a href="/mine/lost"><input type="button" value="확인"></a>
+	</form>
+<%
+	}
+%>
 </body>
 </html>
