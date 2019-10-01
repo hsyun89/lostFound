337c675886e180f3736b75c276b01116a909f414
hsyun89 <hsyun89@naver.com>
2019-10-01 20:45:33 +0900

    webSocket

--- src/main/java/my/spring/mine/AuctionController.java
+++ src/main/java/my/spring/mine/AuctionController.java
@@ -1,5 +1,17 @@
 package my.spring.mine;
 
-public class AuctionController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class AuctionController {
+	@RequestMapping(value = "/auction", method = RequestMethod.GET)
+	protected ModelAndView get() {
+		ModelAndView mav = new ModelAndView();
+		String viewName = "websocket-echo";
+		mav.setViewName(viewName);
+		return mav;
+	}
 }
