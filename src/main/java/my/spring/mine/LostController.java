53d64acd4ffdd7f60e4634243269860bae10359e
kyungJun <vndn337@nate.com>
2019-10-21 14:38:00 +0900

    .

--- src/main/java/my/spring/mine/LostController.java
+++ src/main/java/my/spring/mine/LostController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import dao.LostDAO;
-import vo.LostVO;
 import vo.PageMakerAndSearch;
 
 @Controller
